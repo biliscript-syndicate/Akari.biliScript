b9b2ca369b2769ebbb1ce7b101c8ce08af4fba5d
Akaza Akari <tt@a-kar.in>
2013-06-18 19:50:48 +0800

    basic profiling

--- buildVersions.bat
+++ buildVersions.bat
@@ -1,6 +1,6 @@
 @echo off
 build.rb -o "BuildOutput/Akari.biliScript"
-build.rb -o "BuildOutput/Akari.Debug.biliScript" -v "debug" -def "DEBUG=true;"
+build.rb -o "BuildOutput/Akari.Debug.biliScript" -v "debug" -def "DEBUG=true;PERF_VISUAL=true;"
 build.rb -o "BuildOutput/Akari.Essence.biliScript" -v "essence" -def "INCL_ANIM=false;INCL_TEXT=false;INCL_FX=false;INCL_THREE=false;INCL_VECTOR=false;INCL_RAND=false;INCL_SHAND=false;"
 
 build.rb -o "BuildOutput/Akari.min.biliScript" -strip comments
